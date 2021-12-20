58114b1a17d9cadfa9d385792ea29c8033b50daa
Jonathan Sönnerup <Nooxet@gmail.com>
2021-12-20 12:03:26 +0100

    Added express with frontend example, wip

---
+++ Code/express-frontend/moviesdb.js
@@ -0,0 +1,63 @@
+let movies = {
+    "watchList": [
+        {
+            "title": "The Matrix Resurrections",
+            "year": 2021,
+            "rating": null
+        },
+        {
+            "title": "Tiger King",
+            "year": 2020,
+            "rating": null
+        }
+    ],
+    "seen": [
+        {
+            "title": "Breaking Bad",
+            "year": 2008,
+            "rating": 10
+        },
+        {
+            "title": "Shutter Island",
+            "year": 2010,
+            "rating": 8
+        },
+        {
+            "title": "Pulp Fiction",
+            "year": 1994,
+            "rating": 9
+        },
+        {
+            "title": "The Matrix",
+            "year": 1999,
+            "rating": 9
+        },
+        {
+            "title": "Fight Club",
+            "year": 1999,
+            "rating": 9
+        },
+        {
+            "title": "Memento",
+            "year": 2000,
+            "rating": 8
+        },
+        {
+            "title": "Disaster Movie",
+            "year": 2008,
+            "rating": 2
+        },
+        {
+            "title": "Epic Movie",
+            "year": 2007,
+            "rating": 3
+        },
+        {
+            "title": "Daddy Day Camp",
+            "year": 2007,
+            "rating": 3
+        }
+    ]
+}
+
+exports.moviesDB = movies;
---
+++ Code/express-frontend/server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+const port = 1337;
+
+// get the movies map from the "DB" file
+const moviesdb = require("./moviesdb");
+let movies = moviesdb.moviesDB;
+
+app.get("/", function (req, res) {
+    // get the absolute path to the index.html file
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/filterMovieOnRating", function (req, res) {
+    let rating = Number(req.query["filterRating"]);
+    let filteredMovies = filterOnRating(movies["seen"], rating);
+    res.send(filteredMovies);
+});
+
+/**
+ * Filter movies based on their rating, will match exact rating
+ * 
+ * @param {Array} movies - An array of already rated movies
+ * @param {Number} filter - The rating to filter movies on
+ */
+function filterOnRating(movies, filter) {
+    let filtered = [];
+    for (const movie of movies) {
+        if (movie["rating"] === filter) {
+            filtered.push(movie);
+        }
+    }
+
+    return filtered;
+}
+
+app.listen(port, function () {
+    console.log("Web server started!");
+});
