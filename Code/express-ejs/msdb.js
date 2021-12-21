dd2978c0ad6b523cb2ed08df8441cd810db63b62
Jonathan Sönnerup <Nooxet@gmail.com>
2021-12-21 13:19:47 +0100

    Added a web app using express with EJS example, wip

---
+++ Code/express-ejs/msdb.js
@@ -0,0 +1,124 @@
+let ms = {
+    "series": [
+        {
+            "title": "Tiger King",
+            "year": 2020,
+            "rating": null
+        },
+        {
+            "title": "Breaking Bad",
+            "year": 2008,
+            "rating": 10
+        },
+        {
+            "title": "Seinfeld",
+            "year": 1989,
+            "rating": 10
+        },
+        {
+            "title": "Dexter",
+            "year": 2006,
+            "rating": 7
+        },
+        {
+            "title": "The Office",
+            "year": 2005,
+            "rating": 10
+        },
+        {
+            "title": "The Witcher",
+            "year": 2019,
+            "rating": null
+        },
+        {
+            "title": "Parks and Recreation",
+            "year": 2009,
+            "rating": 8
+        },
+        {
+            "title": "House MD",
+            "year": 2004,
+            "rating": 9
+        },
+        {
+            "title": "Squid Game",
+            "year": 2021,
+            "rating": null
+        },
+        {
+            "title": "Brooklyn Nine-Nine",
+            "year": 2013,
+            "rating": 7
+        }
+    ],
+    "movies": [
+        {
+            "title": "The Matrix Resurrections",
+            "year": 2021,
+            "rating": null
+        },
+        {
+            "title": "Shutter Island",
+            "year": 2010,
+            "rating": 9
+        },
+        {
+            "title": "Pulp Fiction",
+            "year": 1994,
+            "rating": 9
+        },
+        {
+            "title": "The Matrix",
+            "year": 1999,
+            "rating": 9
+        },
+        {
+            "title": "Office Christmas Party",
+            "year": 2016,
+            "rating": null
+        },
+        {
+            "title": "Fight Club",
+            "year": 1999,
+            "rating": 9
+        },
+        {
+            "title": "Memento",
+            "year": 2000,
+            "rating": 8
+        },
+        {
+            "title": "Disaster Movie",
+            "year": 2008,
+            "rating": 2
+        },
+        {
+            "title": "Avengers: Endgame",
+            "year": 2019,
+            "rating": null
+        },
+        {
+            "title": "Epic Movie",
+            "year": 2007,
+            "rating": 3
+        },
+        {
+            "title": "Ex Machina",
+            "year": 2014,
+            "rating": null
+        },
+        {
+            "title": "Daddy Day Camp",
+            "year": 2007,
+            "rating": 3
+        },
+        {
+            "title": "Die Hard",
+            "year": 1988,
+            "rating": null
+        }
+    ]
+}
+
+// only necessary when placing this variable in its own js file.
+exports.msDB = ms;
---
+++ Code/express-ejs/server.js
@@ -0,0 +1,65 @@
+const express = require("express");
+
+const app = express();
+const port = 1337;
+
+// Tell express to use EJS to render html
+app.set("view engine", "ejs");
+
+// needed to let express know how to interpret the data sent in the body (when using POST)
+app.use(express.urlencoded({ extended: true }));
+
+// get the movies/series map from the "DB" file
+// Only necessary if we do not declare the variable in this file
+const msdb = require("./msdb");
+let ms = msdb.msDB;
+
+/*
+ * ROUTES
+ */
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/getAll", function (req, res) {
+    /*
+    fs.readFile("dfd.json", function (err, data) {
+        // example of reading file
+        let movies = JSON.parse(data);
+        // do this inside the callback function, due to async function
+        res.send()
+    });
+    */
+
+    // EJS will look in the "views" folder plus the path we give it
+    res.render("pages/getall", { movies: ms["movies"], series: ms["series"] });
+});
+
+app.post("/addMS", function (req, res) {
+    let mstype = req.body["mstype"];
+    let title = req.body["title"];
+    let year = Number(req.body["year"]);
+    let rating = Number(req.body["rating"]);
+
+    let msToAdd = {
+        "title": title,
+        "year": year,
+        "rating": rating
+    };
+
+    if (mstype === "movie") {
+        ms["movies"].push(msToAdd);
+    } else if (mstype === "serie") {
+        ms["series"].push(msToAdd);
+    } else {
+        console.log("This will NEVER happen :)");
+        res.send("WTF!");
+    }
+
+    res.send("Added!");
+});
+
+app.listen(port, function () {
+    console.log("Server started!");
+});
